90184ea75d64f31c3436d404ac8c8ecea26c9694
Jeferson Menegaldo <52977305+jefersonmenegaldo@users.noreply.github.com>
2023-09-18 08:16:20 -0300

    Update sketch.js

--- Pong/sketch.js
+++ Pong/sketch.js
@@ -1,29 +1,41 @@
-let xBolinha = 400;
-let yBolinha = 400;
+let xBolinha = 300;
+let yBolinha = 200;
 let diametro = 15;
+let raio = diametro / 2;
 
-let velocidadeXBolinha = 7;
+let velocidadeXBolinha = 6;
 let velocidadeYBolinha = 6;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
   background(0);
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();  
+}
+
+function mostraBolinha() {
   circle(xBolinha, yBolinha, diametro);
+}
+
+function movimentaBolinha() {
   xBolinha = xBolinha + velocidadeXBolinha;
   yBolinha = yBolinha + velocidadeYBolinha;
-  
+}
+
+function verificaColisaoBorda() { //inicio da função
   //se a bolinha tocar na borda volte 
   // (   verdadeiro    ou   verdadeiro)
-  if (xBolinha > width || xBolinha < 0) {
+  if (xBolinha + raio > width || xBolinha - raio < 0) { //inicio do if ou Se
     //velocidade x da bolinha recebe o seu valor atual
     //multiplicado por -1, que inverte o sinal
     velocidadeXBolinha = velocidadeXBolinha * -1;
-  }
+  } // fim do if
 
-  if (yBolinha > height || yBolinha < 0) {
+  if (yBolinha + raio > height || yBolinha - raio < 0) { // inicio do if
     velocidadeYBolinha = velocidadeYBolinha * -1;
-  }
-}
+  } // fim do if
+} // fim da função
