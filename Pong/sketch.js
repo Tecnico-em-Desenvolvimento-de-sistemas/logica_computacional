8d35d76487d59a7066e8955e8e647014e7613782
Jeferson Menegaldo <52977305+jefersonmenegaldo@users.noreply.github.com>
2023-10-09 07:33:50 -0300

    Update sketch.js

--- Pong/sketch.js
+++ Pong/sketch.js
@@ -11,16 +11,20 @@ let yRaquete = 150;
 let raqueteComprimento = 10;
 let raqueteAltura = 90;
 
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+let velocidadeYOponente;
+
 function setup() {
   createCanvas(600, 400);
 }
 
 function movimentaRaquete() {
   if (keyIsDown(UP_ARROW)) {
-    yRaquete = yRaquete - 10;  
+    yRaquete = yRaquete - 10;
   }
   if (keyIsDown(DOWN_ARROW)) {
-    yRaquete = yRaquete + 10;  
+    yRaquete = yRaquete + 10;
   }
 }
 
@@ -28,9 +32,27 @@ function draw() {
   background(0);
   mostraBolinha();
   movimentaBolinha();
-  verificaColisaoBorda();  
+  verificaColisaoBorda();
   mostraRaquete();
   movimentaRaquete();
+  verificaColisaoRaquete();
+  mostraRaqueteOponente();
+  movimentaRaqueteOponente();
+}
+
+function verificaColisaoRaquete() {
+  if (xBolinha - raio < xRaquete + raqueteComprimento) {
+    velocidadeXBolinha = velocidadeXBolinha * -1;
+  }
+}
+
+function movimentaRaqueteOponente() {
+    velocidadeYOponente = yBolinha - yRaqueteOponente - raqueteComprimento / 2 - 30;
+    yRaqueteOponente += velocidadeYOponente
+}
+
+function mostraRaqueteOponente() {
+  rect(xRaqueteOponente, yRaqueteOponente, raqueteComprimento, raqueteAltura);
 }
 
 function mostraRaquete() {
@@ -46,16 +68,19 @@ function movimentaBolinha() {
   yBolinha = yBolinha + velocidadeYBolinha;
 }
 
-function verificaColisaoBorda() { //inicio da função
-  //se a bolinha tocar na borda volte 
+function verificaColisaoBorda() {
+  //inicio da função
+  //se a bolinha tocar na borda volte
   // (   verdadeiro    ou   verdadeiro)
-  if (xBolinha + raio > width || xBolinha - raio < 0) { //inicio do if ou Se
+  if (xBolinha + raio > width || xBolinha - raio < 0) {
+    //inicio do if ou Se
     //velocidade x da bolinha recebe o seu valor atual
     //multiplicado por -1, que inverte o sinal
     velocidadeXBolinha = velocidadeXBolinha * -1;
   } // fim do if
 
-  if (yBolinha + raio > height || yBolinha - raio < 0) { // inicio do if
+  if (yBolinha + raio > height || yBolinha - raio < 0) {
+    // inicio do if
     velocidadeYBolinha = velocidadeYBolinha * -1;
   } // fim do if
 } // fim da função
